90d63fbdb6f0c22346e76b4840440ca83a18a511
MisterUniverse <desmondharris912@gmail.com>
2018-03-17 05:30:44 -0500

    Celsius to Fahrenheit converter
    Write a function that converts Celsius to Fahrenheit.

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+// Write a function that converts Celsius to Fahrenheit.
+
+```function celsiusToFahrenheit(celsius) {
+	let tempCel = celsius * 9 / 5 + 32;
+	return tempCel;
+}
+celsiusToFahrenheit(30);
+```
